dccc454722a8452e1abc40b86d0575c3d25f22fb
Jakob Lindskog <jakob.lindskog@gmail.com>
2021-12-11 18:01:19 +0100

    migrate to vite

---
+++ web/src/components/Song.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="
+      text-center
+      p-2
+      cursor-pointer
+      rounded
+      overflow-hidden
+      hover:shadow-lg
+      h-32
+      border-2 border-gray-200
+    "
+  >
+    <p class="font-bold mb-2">{{ song.title }}</p>
+    <div class="text-xs whitespace-pre h-full truncate">
+      {{ song.text }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['song']
+}
+</script>
