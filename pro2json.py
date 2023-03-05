f6c7f1766ed3f08e7478f87dffc44aea9e97aa44
Jakob Lindskog <jakob.lindskog@gmail.com>
2023-03-05 18:36:17 +0100

    Add date-swaps
    To correct wrong dates in the input data

--- pro2json.py
+++ pro2json.py
@@ -70,7 +70,7 @@ def songs_tgz_to_songs(songs_tgz) -> list:
     return sorted(songs, key=lambda x: x["title"])
 
 
-def pro6pl_to_playlists(pro6pl_filename) -> list:
+def pro6pl_to_playlists(pro6pl_filename, date_swaps) -> list:
     root = ElementTree.parse(pro6pl_filename).getroot()
 
     playlists = []
@@ -78,6 +78,9 @@ def pro6pl_to_playlists(pro6pl_filename) -> list:
     for playlist in root.findall(".//RVPlaylistNode"):
         playlist_name = playlist.get("displayName")
         playlist_date = playlist.get("modifiedDate")
+        if playlist_date in date_swaps:
+            playlist_date = date_swaps[playlist_date]
+
         songs = []
         for song in playlist.findall("array/RVDocumentCue"):
             song_name = song.get("displayName")
@@ -96,10 +99,20 @@ def pro6pl_to_playlists(pro6pl_filename) -> list:
     return sorted(playlists, key=lambda x: x["playlist_date"])
 
 
-def main(pro6pl_filename, songs_tgz, outdir):
+def read_json(filename):
+    with open(filename) as f:
+        return json.load(f)
+
+
+def main(pro6pl_filename, songs_tgz, date_swaps_json, outdir):
     os.makedirs(outdir, exist_ok=True)
 
-    playlists = pro6pl_to_playlists(pro6pl_filename)
+    if date_swaps_json:
+        date_swaps = read_json(date_swaps_json)
+    else:
+        date_swaps = {}
+
+    playlists = pro6pl_to_playlists(pro6pl_filename, date_swaps)
     if len(playlists) == 0:
         print("WARN: you have no playlists")
     write_json(playlists, f"{outdir}/playlists.json")
@@ -117,5 +130,6 @@ if __name__ == "__main__":
     parser.add_argument("--outdir", required=True)
     parser.add_argument("--playlist_pro6pl", required=True)
     parser.add_argument("--songs_tgz", required=True)
+    parser.add_argument("--date-swaps", default=None)
     args = parser.parse_args()
-    main(args.playlist_pro6pl, args.songs_tgz, args.outdir)
+    main(args.playlist_pro6pl, args.songs_tgz, args.date_swaps, args.outdir)
