45dfb4e64ddf4800fa0b4d46f3d9ae0c151daa3b
Jakob Lindskog <jakob.lindskog@gmail.com>
2023-03-05 18:40:36 +0100

    new font: Inter

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -13,6 +13,18 @@ module.exports = {
       minWidth: {
         '80': '20rem',
       },
+      fontFamily: {
+        'sans': "Inter, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
+      },
+      animation: {
+        'yellow2white': 'yellow2white 1.5s linear'
+      },
+      keyframes: {
+        yellow2white: {
+          '0%': { background: 'rgba(255,255,150)' },
+          '100%': { background: '#fff' }
+        }
+      }
     },
   },
   plugins: [],
